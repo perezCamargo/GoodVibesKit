08cfca0b3aa0e7fa53eb5dbdbf8596301cba55af
Felipe Perez <felipe.perez@accedo.tv>
2019-06-26 14:20:00 -0400

    small access fix

--- Sources/GoodVibesKit/GoodVibesKit.swift
+++ Sources/GoodVibesKit/GoodVibesKit.swift
@@ -1,13 +1,13 @@
 public struct GoodVibesKit {
     public init() { }
-    public var text = "Hello, welcome to Good Vibes Kit!"
-    public var drink = "This is a drink filled with good vibes 🥃!"
+    public var welcome = "Hello, welcome to Good Vibes Kit!"
+    public var basic = "This is a drink filled with good vibes 🥃!"
     
     internal var drinks = ["🥃","🥛","🥤","🍻","🍾","🍵","🍶","🍷","🍸","🍹","🍺","🍼","☕️"]
     
     public func getGoodVibeDrink() -> String {
         
-        return self.drinks[Int.random(in: 0..<drink.count)]
+        return self.drinks[Int.random(in: 0..<drinks.count)]
     }
     
 }
